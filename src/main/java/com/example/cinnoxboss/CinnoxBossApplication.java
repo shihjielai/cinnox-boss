004d1f50e0cad728fa5236cef660b6f138e921df
Jay <shihjielai@gmail.com>
2022-10-16 17:16:20 +0800

    init

---
+++ src/main/java/com/example/cinnoxboss/CinnoxBossApplication.java
@@ -0,0 +1,13 @@
+package com.example.cinnoxboss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinnoxBossApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinnoxBossApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cinnoxboss/CinnoxBossApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cinnoxboss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinnoxBossApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
